Fix memory overflow

deep_ls/data.py
@@ -79,37 +79,38 @@ def preprocess_samples(samples, grid_indices, radius=0.0625):
     xyz = samples[:,:3]
     sdf_tree = cKDTree(xyz)
     max_size = 300000
+    with torch.no_grad():
+        for center_point_index in range(0, len(grid_indices), batch_size):
+            index = center_point_index
 
-    for center_point_index in range(0, len(grid_indices), batch_size):
-        index = center_point_index
+            # Get all indices of the samples that are within the L-radius around the cell center.
+            near_sample_indices = sdf_tree.query_ball_point(x=[grid_indices[index]], r=radius, p=np.inf, return_sorted=True)
+            # Get number of samples located samples within the L-radius around the cell center
+            near_sample_indices = near_sample_indices[0]
+            #near_sample_indices_single = near_sample_indices_single[0]
+            if len(near_sample_indices) < 1: 
+                continue
+            sdf_gt = samples[near_sample_indices, 3]
+            
+            temp_sdf_gts.extend(torch.tanh(sdf_gt))
 
-        # Get all indices of the samples that are within the L-radius around the cell center.
-        near_sample_indices = sdf_tree.query_ball_point(x=[grid_indices[index]], r=radius, p=np.inf, return_sorted=True)
-        # Get number of samples located samples within the L-radius around the cell center
-        near_sample_indices = near_sample_indices[0]
-        #near_sample_indices_single = near_sample_indices_single[0]
-        if len(near_sample_indices) < 1: 
-            continue
-        sdf_gt = samples[near_sample_indices, 3]
-        
-        temp_sdf_gts.extend(torch.tanh(sdf_gt))
-
-        transformed_samples = samples[near_sample_indices, :3] - grid_indices[index] 
-        #transformed_samples = transformed_samples.expand((near_sample_indices, 3))
-        temp_inputs.extend(transformed_samples)
-        center_index = np.repeat(center_point_index, len(near_sample_indices))
-        temp_input_grid_indices.extend(torch.from_numpy(center_index))
-    padding = max_size - len(temp_input_grid_indices)
-    temp_input_grid_indices = torch.stack(temp_input_grid_indices)
-    zeros_input_grid_indices = torch.zeros_like(temp_input_grid_indices[0])
-    temp_input_grid_indices = torch.cat([temp_input_grid_indices, zeros_input_grid_indices.repeat(padding)])
-    temp_inputs = torch.stack(temp_inputs) 
-    zeros_inputs = torch.zeros_like(temp_inputs[0, :]).expand(1, 3)
-    temp_inputs = torch.cat([temp_inputs, zeros_inputs.repeat(padding, 1)])
-    temp_sdf_gts = torch.stack(temp_sdf_gts)
-    zeros_gt = torch.zeros_like(temp_sdf_gts[0])
-    temp_sdf_gts = torch.cat([temp_sdf_gts, zeros_gt.repeat(padding)])
-    return temp_input_grid_indices, temp_inputs, temp_sdf_gts, padding
+            transformed_samples = samples[near_sample_indices, :3] - grid_indices[index] 
+            #transformed_samples = transformed_samples.expand((near_sample_indices, 3))
+            temp_inputs.extend(transformed_samples)
+            center_index = np.repeat(center_point_index, len(near_sample_indices))
+            temp_input_grid_indices.extend(torch.from_numpy(center_index))
+        del sdf_tree
+        padding = max_size - len(temp_input_grid_indices)
+        temp_input_grid_indices = torch.stack(temp_input_grid_indices)
+        zeros_input_grid_indices = torch.zeros_like(temp_input_grid_indices[0])
+        temp_input_grid_indices = torch.cat([temp_input_grid_indices, zeros_input_grid_indices.repeat(padding)])
+        temp_inputs = torch.stack(temp_inputs) 
+        zeros_inputs = torch.zeros_like(temp_inputs[0, :]).expand(1, 3)
+        temp_inputs = torch.cat([temp_inputs, zeros_inputs.repeat(padding, 1)])
+        temp_sdf_gts = torch.stack(temp_sdf_gts)
+        zeros_gt = torch.zeros_like(temp_sdf_gts[0])
+        temp_sdf_gts = torch.cat([temp_sdf_gts, zeros_gt.repeat(padding)])
+        return temp_input_grid_indices, temp_inputs, temp_sdf_gts, padding
 
 
 def unpack_sdf_samples(filename, subsample=None, grid_indices=None):
@@ -129,6 +130,7 @@ def unpack_sdf_samples(filename, subsample=None, grid_indices=None):
     sample_neg = torch.index_select(neg_tensor, 0, random_neg)
 
     samples = torch.cat([sample_pos, sample_neg], 0)
+    grid_indices = generate_grid_center_indices(cube_size=32, box_size=1)
 
     input_grid_indices, inputs, groundtruths, padding = preprocess_samples(samples, grid_indices)
 
@@ -186,8 +188,6 @@ class SDFSamples(torch.utils.data.Dataset):
         self.data_source = data_source
         self.npyfiles = get_instance_filenames(data_source, split)
 
-        self.grid_indices = generate_grid_center_indices(cube_size=32, box_size=1)
-
         logging.debug(
             "using "
             + str(len(self.npyfiles))
@@ -224,4 +224,4 @@ class SDFSamples(torch.utils.data.Dataset):
                 idx,
             )
         else:
-            return unpack_sdf_samples(filename, self.subsample, self.grid_indices), idx
+            return unpack_sdf_samples(filename, self.subsample), idx

train_deep_ls.py
@@ -22,7 +22,9 @@ from scipy.spatial import cKDTree
 from Plot_3d import plot_3D
 import numpy as np
 
+import gc
 from tqdm import tqdm
+
 
 if not sys.warnoptions:
     warnings.simplefilter("ignore")
@@ -395,6 +397,7 @@ def main_function(experiment_directory, continue_from, batch_split):
         shuffle=True,
         num_workers=num_data_loader_threads,
         drop_last=True,
+        pin_memory=True
     )
 
     # Generate grid indices of cube
@@ -523,12 +526,9 @@ def main_function(experiment_directory, continue_from, batch_split):
         max_size = 0
 
         for (sdf_data, input_grid_indices, inputs, groundtruths, padding), indices in tqdm(sdf_loader):
-            # Get correct lat_vecs embedding and load to cuda
-            
             # Sdf_data contains n samples per scene
             #sdf_data = sdf_data.reshape(-1, 31).detach()
             optimizer_all.zero_grad()
-            
             batch_loss = 0.0
             for i in range(len(indices)):
                 temp_lat_vec = lat_vecs[indices[i]]
@@ -541,19 +541,25 @@ def main_function(experiment_directory, continue_from, batch_split):
                     max_size = groundtruth.shape[0]
                 chunk_loss = loss_l1(pred.squeeze(-1), groundtruth.cuda()) / groundtruth.shape[0]
                 chunk_loss.backward()
-                batch_loss += chunk_loss.item()
-            if do_code_regularization:
-                for grid_indx in range(len(sdf_grid_indices)):
-                    code = temp_lat_vec(torch.tensor(grid_indx).long())
-                    l2_size_loss = torch.sum(torch.norm(code, dim=0))
+                batch_loss += chunk_loss.item() / len(indices)
+                if do_code_regularization:
+                    for grid_indx in range(len(sdf_grid_indices)):
+                        code = temp_lat_vec(torch.tensor(grid_indx).long())
+                        l2_size_loss = torch.sum(torch.norm(code.cuda(), dim=0))
 
-                    reg_loss = (code_reg_lambda * min(1.0, epoch / 100) * l2_size_loss) / len(sdf_grid_indices)
-                    batch_loss += reg_loss.cuda()
-                
-            current_scene += 1
-            all_scenes_loss += batch_loss
+                        reg_loss = (code_reg_lambda * min(1.0, epoch / 100) * l2_size_loss) / len(sdf_grid_indices)
+                        batch_loss += reg_loss.cuda()
+            batch_loss = batch_loss.detach()
             loss_log.append(batch_loss)
+            all_scenes_loss += batch_loss
+            current_scene += 1
             optimizer_all.step()
+            del input_grid_indices
+            del inputs
+            del groundtruths
+            del padding
+            del indices
+            gc.collect()
             torch.cuda.empty_cache()
             # zs = plot_xyz[:, 3]
             # zs = np.interp(zs, (zs.min(), zs.max()), (-1, +1))
@@ -610,7 +616,7 @@ def main_function(experiment_directory, continue_from, batch_split):
         end = time.time()
         seconds_elapsed = end - start
         timing_log.append(seconds_elapsed)
-        avg_loss = all_scenes_loss / current_scene
+        avg_loss = all_scenes_loss / len(sdf_loader)
         logging.info("Avg Loss: {}, Epoch took {} seconds.".format(avg_loss, seconds_elapsed))  
         logging.debug("Max size {}".format(max_size))    
 